69c4922167e0830306cbf53fb6a14ab3b60fb494
Natasha Nduku <natashanduku0@gmail.com>
2024-11-22 13:59:05 +0300

    acting on events

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+
+function moveDodgerLeft() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+
+function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left < 360) { // 360 ensures the dodger doesn't move out of the 400px game field
+      dodger.style.left = `${left + 1}px`;
+    }
+  }
+
+  document.addEventListener("keydown", function (event) {
+    if (event.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+    if (event.key === "ArrowRight") {
+      moveDodgerRight();
+    }
+  });
+  
+
+  
